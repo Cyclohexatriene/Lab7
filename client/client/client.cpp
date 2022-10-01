5fada6669a0889d048c99d903bab1480e5f83ff6
dzcdzcdzc <3200104203@zju.edu.cn>
2022-10-01 16:49:20 +0800

    实现了基本功能，但服务器一次仅可处理单个客户端。

--- client/client/client.cpp
+++ client/client/client.cpp
@@ -5,6 +5,23 @@
 #pragma comment (lib,"ws2_32.lib")
 using namespace std;
 
+inline void showMenu1() {
+	cout
+		<< "Please select a fuction: " << endl
+		<< "Input 1 : Connect to server" << endl
+		<< "Input 2 : Quit" << endl;
+}
+
+inline void showMenu2() {
+	cout<<endl
+		<< "Please select a function: " << endl
+		<< "Input 1 : Get server time" << endl
+		<< "Input 2 : Get server name" << endl
+		<< "Input 3 : Get list of active clients" << endl
+		<< "Input 4 : Send message to another client" << endl
+		<< "Input 5 : Disconnect and exit" << endl;
+}
+
 int main()
 {
 	//Initialize DLL
@@ -14,28 +31,94 @@ int main()
 		return -1;
 	}
 
+	int state;
+	while (1) {
+		showMenu1();
+		cin >> state;
+		if (state == 1) break;
+		else if (state == 2) return 0;
+		else cout << "Invalid input. Please input an number again." << endl;
+	}
+
 	//Create socket
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
+	
 	//Request server
 	sockaddr_in sockAddr;
 	memset(&sockAddr, 0, sizeof(sockAddr));
 	sockAddr.sin_family = AF_INET;
-	string serverIP = "127.0.0.1";
-	u_short portNumber = 4203;
+	string serverIP;
+	u_short portNumber;
+	cout << "Please input server IP." << endl;
+	cin >> serverIP;
+	cout << "Please input port number." << endl;
+	cin >> portNumber;
 	inet_pton(AF_INET, serverIP.c_str(), (void*)&sockAddr.sin_addr);
 	sockAddr.sin_port = htons(portNumber);
 	if (!( connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR)) == 0 )) {
 		cerr << "Failed to connect to server. The process will exit with code -1.";
 		return -1;
 	};
+	cout << "Successfully connected to the server." << endl;
+	cout << endl;
 
-	//Get data from server
-	char szBuffer[MAXBYTE] = { 0 };
-	recv(sock, szBuffer, MAXBYTE, 0);
-
-	//Output message
-	cout << "Message from server: " << szBuffer << endl;
+	while (1) {
+		showMenu2();
+		cin >> state;
+		if (state == 1) {
+			//Get server time
+			string msg = "1";
+			send(sock, msg.c_str(), msg.length() + sizeof(char), 0);
+			char szBuffer[MAXBYTE] = { 0 };
+			recv(sock, szBuffer, MAXBYTE, 0);
+			cout << "The server time is: " << szBuffer << endl;
+		}
+		else if (state == 2) {
+			//Get server name
+			string msg = "2";
+			send(sock, msg.c_str(), msg.length() + sizeof(char), 0);
+			char szBuffer[MAXBYTE] = { 0 };
+			recv(sock, szBuffer, MAXBYTE, 0);
+			
+			cout << "The server name is: ";
+			wcout << (WCHAR*)szBuffer << endl;
+		}
+		else if (state == 3) {
+			//Get list of active clients
+			string msg = "3";
+			send(sock, msg.c_str(), msg.length() + sizeof(char), 0);
+			char szBuffer[MAXBYTE] = { 0 };
+			recv(sock, szBuffer, MAXBYTE, 0);
+			cout << "The list of active clients is: "<< endl << szBuffer << endl;
+		}
+		else if (state == 4) {
+			//Send message to another client
+			string msg = "4";
+			cout << "Please input number of target client: " << endl;
+			string target;
+			cin >> target;
+			msg += target+" ";
+			cout << "Please input your message and end with an empty line." << endl;
+			string temp;
+			getline(cin, temp); //Get the rest '\n'
+			while (1) {
+				getline(cin, temp);
+				if (temp.empty()) break;
+				else msg += temp + '\n';
+			}
+			send(sock, msg.c_str(), msg.length() + sizeof(char), 0);
+			char szBuffer[MAXBYTE] = { 0 };
+			recv(sock, szBuffer, MAXBYTE, 0);
+			cout << szBuffer << endl;
+		}
+		else if (state == 5) {
+			//Disconnect and exit
+			string msg = "5";
+			send(sock, msg.c_str(), msg.length() + sizeof(char), 0);
+			break;
+		}
+		else cout << "Invalid input. Please input an number again." << endl;
+	}
 
 	//Close socket
 	closesocket(sock);
--- server/server/server.cpp
+++ server/server/server.cpp
@@ -1,9 +1,22 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 #include <winsock2.h>
 #include <WS2tcpip.h> //definition of function inet_pton() 
+#include <Windows.h>
+#include <sstream>
 #pragma comment (lib,"ws2_32.lib")
 using namespace std;
+
+string getTime() {
+    time_t timep;
+    time(&timep);
+    char tmp[64];
+    struct tm nowTime;
+    localtime_s(&nowTime, &timep);
+    strftime(tmp, sizeof(tmp), "%Y-%m-%d-%H:%M:%S", &nowTime);
+    return string(tmp);
+}
 
 int main()
 {
@@ -28,17 +41,68 @@ int main()
     
     //Start listening from network.
     listen(servSock, 20);
-    int cnt = 0;
-    while (cnt < 2) {
-        SOCKADDR clntAddr;
-        int nSize = sizeof(SOCKADDR);
-        SOCKET clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
-        //Send messages to client.
-        string msg = "Hello World!";
-        send(clntSock, msg.c_str(), msg.length() + sizeof(char), 0);
-        closesocket(clntSock);
-        cnt++;
+
+    vector<pair<SOCKET*,SOCKADDR*>> activeClients; //Store activeClients
+    
+   //Create client socket
+    SOCKADDR clntAddr;
+    int nSize = sizeof(SOCKADDR);
+    SOCKET clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
+    activeClients.push_back({ &clntSock,(SOCKADDR*)&clntAddr });
+    cout << "Received a request." << endl;
+
+    //Answer requests
+    while(1){
+        char szBuffer[MAXBYTE] = { 0 };
+        recv(clntSock, szBuffer, MAXBYTE, 0);
+        if (szBuffer[0] == '1') {
+            string nowTime = getTime();
+            send(clntSock, nowTime.c_str(), nowTime.length() + sizeof(char), 0);
+        }
+        else if (szBuffer[0] == '2') {
+            WCHAR computerName[256];
+            u_long size = 256;
+            GetComputerName(computerName, &size);
+            send(clntSock, (char*)computerName, size*2, 0);
+        }
+        else if (szBuffer[0] == '3') {
+            string msg = "Seq\tAddress\t\tPort\n";
+            for (int i = 0; i < activeClients.size(); i++) {
+                msg += '0' + i;
+                msg += "\t";
+                IN_ADDR ip = ((SOCKADDR_IN*)activeClients[i].second)->sin_addr;
+                USHORT port = ((SOCKADDR_IN*)activeClients[i].second)->sin_port;
+                char ipdotdec[20];
+                inet_ntop(AF_INET, (void*)&ip, ipdotdec, sizeof(ipdotdec));
+                msg += ipdotdec;
+                msg += "\t";
+                msg += to_string(ntohs(port));
+                msg += "\n";
+            }
+            send(clntSock, msg.c_str(), msg.length() + sizeof(char), 0);
+        }
+        else if (szBuffer[0] == '4') {
+            string rcvmsg(szBuffer);
+            string targetstr;
+            int i = 1;
+            for (; rcvmsg[i] != ' '; i++) targetstr += rcvmsg[i];
+            string sdmsg = rcvmsg.substr(i+1);
+            int target = stoi(targetstr);
+            send(*activeClients[target].first, sdmsg.c_str(), sdmsg.length() + sizeof(char), 0);
+        }
+        else if (szBuffer[0] == '5') {
+            cout << "A client disconnected." << endl;
+            break;
+        }
     }
+    //Disconnect
+    for (auto i = activeClients.begin(); i != activeClients.end(); i++) {
+        if (i->first == &clntSock) {
+            activeClients.erase(i);
+            break;
+        }
+    }
+    closesocket(clntSock);
     //Close socket
     closesocket(servSock);
 
